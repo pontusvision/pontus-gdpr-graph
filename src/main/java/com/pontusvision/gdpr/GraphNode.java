a65b29e80b68abb8afafc7b91cc0839b6e486392
Leonardo Martins <lmartins@pontusnetworks.com>
2017-12-24 18:05:33 +0000

    playing wiht height width

--- src/main/java/com/pontusvision/gdpr/GraphNode.java
+++ src/main/java/com/pontusvision/gdpr/GraphNode.java
@@ -73,7 +73,7 @@ public class GraphNode
     }
 
     StringBuilder svgHeadSb = new StringBuilder(
-        "<svg xmlns=\"http://www.w3.org/2000/svg\" preserveAspectRatio=\"xMidYMid meet\" height=\"1200\" width=\"600\" viewPort=\"0 0 1200 600\" style=\"overflow: visible; background: #595959\">")
+        "<svg xmlns=\"http://www.w3.org/2000/svg\" preserveAspectRatio=\"xMidYMid meet\" height=\"1200\" width=\"600\" viewPort=\"0 0 1200 600\" >")
         .append("<foreignObject height=\"100%\" width=\"100%\"  fill=\"#797979\" stroke-width=\"20\" stroke=\"#ffffff\"  >");
 
     StringBuilder tableBodySb = new StringBuilder()
@@ -87,7 +87,7 @@ public class GraphNode
         .append(".tg .tg-x9s4{font-weight:bold;background-color:#9b9b9b;vertical-align:top}")
         .append(".tg .tg-yw4l{vertical-align:top}")
         .append("</style>")
-        .append("<div class=\"tg-wrap\"><table class=\"tg\" style=\"height: calc(100%); width: 600px\">")
+        .append("<div class=\"tg-wrap\"><table class=\"tg\" style=\"  style=\"overflow: visible; background: #595959\" height: 1200px; width: 600px\">")
         .append("<colgroup> <col style=\"width: 30%\"/><col style=\"width: 70%\"/></colgroup>")
         .append("<tr><th class=\"tg-ygl1\">Property</th><th class=\"tg-x9s4\">Value</th></tr>")
         .append(sb)
