d7e68d852a9f0ffcebd3f6219a0700c976210cbb
Leonardo Martins <lmartins@pontusnetworks.com>
2018-02-18 12:44:03 +0000

    fixed issue with globals.groovy, where the base64 was not correctly invoked

--- conf/globals.groovy
+++ conf/globals.groovy
@@ -2357,7 +2357,7 @@ def createNotificationTemplates() {
         g.addV("Object.Notification_Templates")
                 .property("Metadata.Type", "Object.Notification_Templates")
                 .property("Object.Notification_Templates.Id", "SAR READ TEMPLATE")
-                .property("Object.Notification_Templates.Text","<p>Dear {{ context.Person_Title | capitalize }} {{ context.Person_Last_Name |capitalize }}, </p>\n" +
+                .property("Object.Notification_Templates.Text",("<p>Dear {{ context.Person_Title | capitalize }} {{ context.Person_Last_Name |capitalize }}, </p>\n" +
                 "\n" +
                 "  Please find below a summary of the data we HOLD about you:\n" +
                 "  \n" +
@@ -2378,7 +2378,7 @@ def createNotificationTemplates() {
                 "  {{  \"<tr style='border: 1px solid #dddddd;text-align: left;padding: 8px;'><td style='border: 1px solid #dddddd;text-align: left;padding: 8px;'>%s</td><td style='border: 1px solid #dddddd;text-align: left;padding: 8px;'>%s</td>\" | format (key , value )}}\n" +
                 "  {% endfor %}\n" +
                 "  {{ \"</table>\" }}\n" +
-                "  {% endfor %}".bytes.encodeBase64().toString())
+                "  {% endfor %}").bytes.encodeBase64().toString())
                 .property("Object.Notification_Templates.Types", "Person")
                 .property("Object.Notification_Templates.URL", "https://localhost:18443/get_sar_read")
                 .property("Object.Notification_Templates.Label", "SAR Read")
@@ -2387,7 +2387,7 @@ def createNotificationTemplates() {
         g.addV("Object.Notification_Templates")
                 .property("Metadata.Type", "Object.Notification_Templates")
                 .property("Object.Notification_Templates.Id", "SAR UPDATE TEMPLATE")
-                .property("Object.Notification_Templates.Text", "<p>Dear {{ context.Person_Title | capitalize }} {{ context.Person_Last_Name |capitalize }}, </p>\n" +
+                .property("Object.Notification_Templates.Text", ("<p>Dear {{ context.Person_Title | capitalize }} {{ context.Person_Last_Name |capitalize }}, </p>\n" +
                 "\n" +
                 "  Please find below a summary of your personal data that we can UPDATE:\n" +
                 "  \n" +
@@ -2408,7 +2408,7 @@ def createNotificationTemplates() {
                 "  {{  \"<tr style='border: 1px solid #dddddd;text-align: left;padding: 8px;'><td style='border: 1px solid #dddddd;text-align: left;padding: 8px;'>%s</td><td style='border: 1px solid #dddddd;text-align: left;padding: 8px;'>%s</td>\" | format (key , value )}}\n" +
                 "  {% endfor %}\n" +
                 "  {{ \"</table>\" }}\n" +
-                "  {% endfor %}".bytes.encodeBase64().toString())
+                "  {% endfor %}").bytes.encodeBase64().toString())
                 .property("Object.Notification_Templates.Types", "Person")
                 .property("Object.Notification_Templates.URL", "https://localhost:18443/get_sar_update")
                 .property("Object.Notification_Templates.Label", "SAR Update")
@@ -2417,7 +2417,7 @@ def createNotificationTemplates() {
         g.addV("Object.Notification_Templates")
                 .property("Metadata.Type", "Object.Notification_Templates")
                 .property("Object.Notification_Templates.Id", "SAR DELETE TEMPLATE")
-                .property("Object.Notification_Templates.Text", "<p>Dear {{ context.Person_Title | capitalize }} {{ context.Person_Last_Name |capitalize }}, </p>\n" +
+                .property("Object.Notification_Templates.Text", ("<p>Dear {{ context.Person_Title | capitalize }} {{ context.Person_Last_Name |capitalize }}, </p>\n" +
                 "\n" +
                 "  Please find below a summary of your personal data we can delete:\n" +
                 "  \n" +
@@ -2438,7 +2438,7 @@ def createNotificationTemplates() {
                 "  {{  \"<tr style='border: 1px solid #dddddd;text-align: left;padding: 8px;'><td style='border: 1px solid #dddddd;text-align: left;padding: 8px;'>%s</td><td style='border: 1px solid #dddddd;text-align: left;padding: 8px;'>%s</td>\" | format (key , value )}}\n" +
                 "  {% endfor %}\n" +
                 "  {{ \"</table>\" }}\n" +
-                "  {% endfor %}".bytes.encodeBase64().toString())
+                "  {% endfor %}").bytes.encodeBase64().toString())
                 .property("Object.Notification_Templates.Types", "Person")
                 .property("Object.Notification_Templates.URL", "https://localhost:18443/get_sar_delete")
                 .property("Object.Notification_Templates.Label", "SAR Delete")
