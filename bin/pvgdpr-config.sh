93d19cabc242d79ab46977d9d0dd8516a0755975
Pontus <contrib@pontusnetworks.com>
2018-03-31 01:55:17 +0100

    added missing kerberos props

--- bin/pvgdpr-config.sh
+++ bin/pvgdpr-config.sh
@@ -1,4 +1,4 @@
 PVGDPR_DISABLE_HADOOP_CLASSPATH_LOOKUP=true
-PVGDPR_CLASSPATH=/opt/graphdb/current/conf
+PVGDPR_CLASSPATH=/opt/pontus/pontus-hbase/current/conf
 #PVGDPR_OPTS="-Djava.security.auth.login.config=/opt/graphdb/current/conf/jaas.conf -Dsun.security.krb5.debug=true"
 PVGDPR_OPTS="-Djava.security.auth.login.config=/opt/pontus/pontus-graph/current/conf/jaas.conf"
