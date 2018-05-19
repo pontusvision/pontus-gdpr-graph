63d27355d2fcf032ab55dfe609bb4c81aec31d1c
Pontus <contrib@pontusnetworks.com>
2018-05-19 14:39:53 +0100

    modified config file to match LDAP user creation capabilities

--- bin/pvgdpr-config.sh
+++ bin/pvgdpr-config.sh
@@ -1,31 +1,25 @@
 PVGDPR_DISABLE_HADOOP_CLASSPATH_LOOKUP=true
 PVGDPR_CLASSPATH=/opt/pontus/pontus-hbase/current/conf
 #PVGDPR_OPTS="-Djava.security.auth.login.config=/opt/graphdb/current/conf/jaas.conf -Dsun.security.krb5.debug=true"
-PVGDPR_OPTS="-Djava.security.auth.login.config=/opt/pontus/pontus-graph/current/conf/jaas.conf"
-PVGDPR_DISABLE_HADOOP_CLASSPATH_LOOKUP=true
-PVGDPR_CLASSPATH=/opt/graphdb/default/current/conf
-#PVGDPR_OPTS="-Djava.security.auth.login.config=/opt/graphdb/default/current/conf/jaas.conf -Dsun.security.krb5.debug=true"
-#PVGDPR_OPTS=" \
-  #-Djava.security.auth.login.config=/opt/pontus/pontus-graph/current/conf/jaas.conf \
-  #-Dldap.create.user=false \
-  #-Dkerberos.authentication=true \
-  #-Dldap.protocol=ldap \
-  #-Dldap.server.hostname=127.0.0.1 \
-  #-Dldap.port=389 \
-  #-Dldap.domain.name=pontusvision.com \
-  #-Dldap.domain.root=CN=Users,DC=pontusvision,DC=com \
-  #-Dldap.user.group=CN=Administrators,CN=Builtin,DC=pontusvision,DC=com \
-  #-Dldap.admin.user=Administrator \
-  #-Dldap.admin.user.pwd=pa55wordpa55wordPASSWD999 \
-  #-Dldap.create.user=false \
-  #-Dshadow.user.keystore.location=/etc/pki/java/jwt_keystore.jks \
-  #-Dshadow.user.keystore.pwd= \
-  #-Dshadow.user.key.alias=jwt_keystore \
-  #-Dshadow.user.key.pwd= \
-  #-Dshadow.user.key.store.type=JKS \
-  #-Dshadow.user.key.algo=HmacSHA512 \
-  #-Dshadow.user.salt.password.enable=false \
-  #-Dkerberos.authentication=true"
-#
+#PVGDPR_OPTS="-Djava.security.auth.login.config=/opt/pontus/pontus-graph/current/conf/jaas.conf"
+PVGDPR_OPTS=" \
+ -Dshadow.user.keystore.location=/etc/pki/java/shadow.jks \
+ -Dshadow.user.keystore.pwd=pa55word \
+ -Dshadow.user.key.pwd=pa55word \
+ -Dshadow.user.key.alias=shadow \
+ -Dshadow.user.key.algo=HmacSHA512 \
+ -Dshadow.user.key.store.type=JCEKS \
+ -Djava.security.auth.login.config=/opt/pontus/pontus-graph/current/conf/jaas.conf \
+ -Dldap.create.user=true  \
+ -Dkerberos.authentication=false  \
+ -Dldap.protocol=ldaps  \
+ -Dldap.server.hostname=localhost  \
+ -Dldap.port=636  \
+ -Dldap.domain.name=pontusvision.com  \
+ -Dldap.domain.root=CN=Users,DC=pontusvision,DC=com  \
+ -Dldap.user.group=CN=Administrators,CN=Builtin,DC=pontusvision,DC=com  \
+ -Dldap.admin.user=Administrator  \
+ -Dldap.admin.user.pwd=pa55wordpa55wordPASSWD999"
+
 CLASS=com.pontusvision.gdpr.App
 #CLASS=uk.gov.cdp.pole.bootstrap.Bootstrap
