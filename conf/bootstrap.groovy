2f28291a2387d8aa0cf5c0e027f55cb7cf8d1efd
Leonardo Martins <lmartins@pontusnetworks.com>
2018-08-09 12:50:13 +0100

    added logic to catch errors and report them whilst loading the schema files; also put logic to skip the loading if the files are not there to speed up the process if the schema is already loaded; also added env var for loading the GDPR SCHEMA -- if PONTUS_GDPR_MODE is set to true in the env vars

--- conf/bootstrap.groovy
+++ conf/bootstrap.groovy
@@ -198,7 +198,12 @@ class Schema {
 }
 
 try {
-    createIndicesPropsAndLabels();
+    String gdprModeEnv = System.getenv("PONTUS_GDPR_MODE");
+    if (gdprModeEnv != null && Boolean.parseBoolean(gdprModeEnv)){
+        createIndicesPropsAndLabels();
+    }
+    loadSchema('/tmp/graphSchema_full.json', '/tmp/graphSchema_ext.json')
+
 } catch (e) {
     e.printStackTrace()
 }
