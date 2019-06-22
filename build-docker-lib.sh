f565b65acab0461296c8bd92c426623566b7b311
Pontus <lmartins@pontusnetworks.com>
2019-06-22 12:04:35 +0100

    split lib and build confs

--- build-docker-lib.sh
+++ build-docker-lib.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-cd $DIR/docker
+cd $DIR/docker-lib
 docker build --no-cache --rm . -t pontusvisiongdpr/pontus-graphdb-lib
 
-cd $DIR/docker-conf
-docker build --no-cache --rm . -t pontusvisiongdpr/pontus-graphdb-conf
-
-docker push pontusvisiongdpr/pontus-graphdb-conf
 docker push pontusvisiongdpr/pontus-graphdb-lib
 
